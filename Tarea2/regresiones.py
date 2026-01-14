f185f10ee6d3a8472e61267f9a36b6618b0bce73
Mateo <mateob-1097@hotmail.com>
2026-01-14 02:00:10 -0500

    Añadiendo documento qmd presentaciones

--- Tarea2/regresiones.py
+++ Tarea2/regresiones.py
@@ -9,7 +9,7 @@ class Inferencia:
     def correlation_heatmap(data, figsize=(20,10), vmin=-1, vmax=1, cmap='BrBG', title='Mapa de calor de correlación triangular', fontsize=18):
         plt.figure(figsize=figsize)
         numeric_data = data.select_dtypes(include=[np.number])
-        mask = np.triu(np.ones_like(numeric_data.corr(), dtype=np.bool))
+        mask = np.triu(np.ones_like(numeric_data.corr(), dtype=bool))  # ← Cambio aquí
         heatmap = sns.heatmap(numeric_data.corr(), mask=mask, vmin=vmin, vmax=vmax, annot=True, cmap=cmap)
         heatmap.set_title(title, fontdict={'fontsize':fontsize}, pad=16)
         plt.show()
